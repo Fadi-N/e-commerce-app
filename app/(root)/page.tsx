9414e5e5217a9afadcbcfc629ce03a17f12b94e2
Fadi-N <fadinachawati17@gmail.com>
2023-07-23 11:34:52 +0200

    Create Store Modal - provider, hook, modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,25 @@
-import {UserButton} from "@clerk/nextjs";
+'use client'
+
+import {useStoreModal} from "@/hooks/use-store-modal";
+import {useEffect} from "react";
 
 const SetupPage = () => {
-  return (
-      <UserButton afterSignOutUrl="/"/>
-  )
+    // We don't declare like this because it doesn't work with useState const storeModal = useStoreModal();
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        // Check if our modal is open, if not open it
+        if (!isOpen) {
+            onOpen();
+        }
+    }, [isOpen, onOpen])
+
+    return (
+        <div className="p-4">
+            Root Page
+        </div>
+    )
 }
 
 export default SetupPage
---
+++ providers/modal-provider.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {StoreModal} from "@/modals/store-modal";
+
+export const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    // If I am on client side set is Mounted as true
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+
+    // If I am on service side rendering return null
+    // This eliminates hydration error
+    if (!isMounted) {
+        return null;
+    }
+
+    // If we are on client side
+    return (
+        <>
+            <StoreModal/>
+        </>
+    )
+}
